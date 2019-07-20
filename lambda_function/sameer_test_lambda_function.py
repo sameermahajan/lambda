137d7a938a009ce15352a81f748c6180b3b4b850
Sameer Mahajan <sameer.s.mahajan@gmail.com>
2019-07-20 20:48:39 +0530

    Update sameer_test_lambda_function.py

--- lambda_function/sameer_test_lambda_function.py
+++ lambda_function/sameer_test_lambda_function.py
@@ -3,7 +3,7 @@ from botocore.vendored import requests
 
 def lambda_handler(event, context):
     id = event['id']
-    base_url = 'http://35.170.242.186:8080/samples'
+    base_url = 'http://<public ip>:8080/samples'
 
     response = requests.get(base_url + '/' + id)
 
